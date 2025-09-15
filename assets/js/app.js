a0c5b1a12dc6cededfd26057e767e4fb9cbf9ccb
Aries Aquino <aries.aquino1109@gmail.com>
2025-09-15 11:11:50 +0800

    Updated project files

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,4 +20,23 @@ document.addEventListener('DOMContentLoaded', () => {
             navLinks.classList.remove('flex');
         }, 300); // 300ms, kasing tagal ng CSS transition
     });
+
+    // On-scroll animation
+    const sections = document.querySelectorAll('.animate-on-scroll');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+                // Optional: Stop observing once it's visible to improve performance
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.2 // Trigger when 20% of the element is visible
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
 });
